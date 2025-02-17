0af8d8c4634ff6a3599e4f1120bda7863d8ac530
anjola oluyale <anjolaoluyale@gmail.com>
2025-02-17 03:13:40 +0100

    Resolved merge conflicts

--- update_portfolio.ps1
+++ update_portfolio.ps1
@@ -1,9 +1,14 @@
 # Define repository path
-$repoPath = "C:\Users\anjol\OneDrive\Documents\github\GitHub-Auto-Update\.git"
+$repoPath = "C:\Users\anjol\OneDrive\Documents\github\GitHub-Auto-Update"
+
 
 # Change to the repository directory
 Set-Location -Path $repoPath
 
+
+# Configure Git (only needed once)
+git config --global user.name "Anjolaoluwa-Oluyale"
+git config --global user.email "anjolaoluyale.com"
 
 # Pull the latest changes (if any)
 git pull origin main
