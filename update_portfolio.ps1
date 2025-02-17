4b11be2c13c2942aa5645077b2522f18eb54013a
Anjolaoluwa-Oluyale <anjolaoluyale@gmail.com>
2025-02-17 03:30:28 +0100

    Updated portfolio on 2025-02-17 03:30:28

--- update_portfolio.ps1
+++ update_portfolio.ps1
@@ -1,8 +1,15 @@
 # Define repository path
 $repoPath = "C:\Users\anjol\OneDrive\Documents\github\GitHub-Auto-Update"
 
+
 # Change to the repository directory
 Set-Location -Path $repoPath
+
+
+# Configure Git (only needed once)
+git config --global user.name "Anjolaoluwa-Oluyale"
+git config --global user.email "anjolaoluyale@gmail.com"
+
 
 # Pull the latest changes (if any)
 git pull origin main
